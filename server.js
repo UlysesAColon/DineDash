f65bd11a838c1be2a1c65bcf1a6ce7a086e36d20
antuic <anthonyferreira1999@gmail.com>
2019-02-25 19:26:42 -0500

    more fun

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ mongoose
 // Use Routes
 app.use('/api/items', items);
 
-  const port = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
 
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
