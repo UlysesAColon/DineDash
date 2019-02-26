eed59b5672acbc2e6f4d60a1d0d8b3c8c13d7dbf
antuic <anthonyferreira1999@gmail.com>
2019-02-25 19:58:07 -0500

    okay

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { Container } from 'reactstrap';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
