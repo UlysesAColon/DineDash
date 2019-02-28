5a88dbc382863d89971ec1566831b533c1bc608a
antuic <anthonyferreira1999@gmail.com>
2019-02-27 20:54:46 -0500

    ok

--- server.js
+++ server.js
@@ -27,13 +27,8 @@ app.use('/api/items', items);
 var path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
 }
-
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
 
 app.get('*', function(req, res){
   res.sendFile(path.resolve('client/public/index.html'));
