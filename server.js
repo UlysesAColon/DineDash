a04870f19eff305a21a6695f4ca86f59c0d47321
antuic <anthonyferreira1999@gmail.com>
2019-02-27 19:31:17 -0500

    lol

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ mongoose
 app.use('/api/items', items);
 
   const port = process.env.PORT || 5000;
+  
+  app.use(express.static(__dirname + '/public'))
 
   app.listen(port, () => console.log(`Server started on port ${port}`));
 
