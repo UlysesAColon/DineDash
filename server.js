e37c50a170f84aafa46f9d2354f5ef0c5d61f382
antuic <anthonyferreira1999@gmail.com>
2019-03-02 12:54:17 -0500

    lol

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname,'client','build','index.html'));
 });
 
-// //build
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/client/public/index.html'));
-// })
+ //build
+ app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname+'/client/public/index.html'));
+ })
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
