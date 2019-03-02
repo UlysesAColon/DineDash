605d78d4eb89df273f0aadbe2a94594f9419c046
antuic <anthonyferreira1999@gmail.com>
2019-03-02 12:34:42 -0500

    update to deploy

--- server.js
+++ server.js
@@ -46,10 +46,10 @@ app.get('*', (req,res) =>{
 });
 }
 
-//build
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
+// //build
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/public/index.html'));
+// })
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
