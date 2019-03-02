a173f15466e7b11f53fca843782c5454ef11ca82
antuic <anthonyferreira1999@gmail.com>
2019-03-02 12:32:54 -0500

    test to deploy

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
 app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname = '/client/build'));
-  });
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 }
 
 //build
