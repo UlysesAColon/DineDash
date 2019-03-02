a9f208d3cefd8998160637b9a2fd63dc5cf6c99a
antuic <anthonyferreira1999@gmail.com>
2019-03-02 12:25:22 -0500

    ok

--- server.js
+++ server.js
@@ -35,14 +35,21 @@ app.use(express.static(path.join(__dirname, 'client/public')));
 //    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 //  });
 //}
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build/index')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname = 'client/build', 'index.html'));
   });
 }
+
+//build
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
