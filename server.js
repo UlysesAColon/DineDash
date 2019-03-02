51cc2fafd4463d4efb440f3f8a1b2a0a62a6c9b0
antuic <anthonyferreira1999@gmail.com>
2019-03-02 11:22:57 -0500

    ok

--- server.js
+++ server.js
@@ -32,13 +32,9 @@ app.use(express.static(path.join(__dirname, 'client/public')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/public/'));
   app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname + '/../client/build'));
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
   });
 }
-
-app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname + '/../client/public/index.html'));
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
